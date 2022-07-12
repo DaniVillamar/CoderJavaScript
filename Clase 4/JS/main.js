fe5f79da4ba5ebb1fb1256cb20a6f5642d7f1cfb
DaniVillamar <villamar959@gmail.com>
2022-07-11 20:25:19 -0500

    Curso JavaScript CoderHouse

---
+++ Clase 4/JS/main.js
@@ -0,0 +1,14 @@
+console.log('Funciones');
+let nombre1= 'Juan';
+let nombre2= 'Lucas';
+let nombre3= 'Daniel';
+
+function saludar(nombre) {
+    console.log('Hola' + nombre);
+    console.log('Haciendo Login.....');
+    console.log('Bienvenido a la App ');
+}
+
+saludar(nombre1);
+saludar(nombre2);
+saludar(nombre3);
--- entregable-01/JS/main.js
+++ entregable-01/JS/main.js
@@ -3,25 +3,58 @@ console.log('Ejemplo estra');
 /*
  */
 
-let comando = prompt(' Que opcion quieres hacer (Promedio, Suma, Multiplicacion)');
+let comando = prompt(' Que Productos deseas Llevar (Frutas, Verduras, Carnes)');
 
 switch (comando) {
-    case 'promedio':
+    case 'Frutas':
 
-        const TOTAL_OPCIONES = prompt('Cuantas calificaciones quieres sacar');
+        const TOTAL_OPCIONES = prompt('Cuantas Frutas vas a llevar: ');
         let total = 0;
         for (let i = 0; i < TOTAL_OPCIONES; i++) {
             console.log('Iteracion' + i);
-            total = total + parseInt(prompt('Dame una calificacion'));
+            nombre = prompt('Nombre de la fruta')
+            total = total + parseInt(prompt('Que precio tiene'));
         }
-        let promedio = total / TOTAL_OPCIONES;
-        alert('El promedio es: ' + promedio);
+        let totalFrutas = total;
+        alert('El Total de tus Frutas es: ' + totalFrutas);
+        if (totalFrutas >= 100) {
+            alert('Ganaste un cumpon para tu siguiente compra del 20%')
+        } else if (totalFrutas < 100) {
+            alert('Lamentablemente no ganastes un cupon por pobre')
+        }
         break;
-    case 'Suma':
+    case 'Verduras':
 
+        const TOTAL_OPCIONES2 = prompt('Cuantas Verduras vas a llevar: ');
+        let total2 = 0;
+        for (let g = 0; g < TOTAL_OPCIONES2; g++) {
+            console.log('Iteracion' + g);
+            nombreV = prompt('Nombre de la Verdura')
+            total2 = total2 + parseInt(prompt('Que precio tiene'));
+        }
+        let totalVerduras = total2;
+        alert('El Total de tus Verduras es: ' + totalVerduras);
+        if (totalVerduras >= 100) {
+            alert('Ganaste un cumpon para tu siguiente compra del 20%')
+        } else if (totalVerduras < 100) {
+            alert('Lamentablemente no ganastes un cupon por pobre')
+        }
         break;
-    case 'Multiplicacion':
-
+    case 'Carnes':
+        const TOTAL_OPCIONES3 = prompt('Cuantas tipos de carne vas a llevar: ');
+        let total3 = 0;
+        for (let g = 0; g < TOTAL_OPCIONES3; g++) {
+            console.log('Iteracion' + g);
+            nombre3 = prompt('Nombre de la Carne')
+            total3 = total3 + parseInt(prompt('Que precio tiene'));
+        }
+        let totalCarnes = total3;
+        alert('El Total de tus Verduras es: ' + totalCarnes);
+        if (totalCarnes >= 100) {
+            alert('Ganaste un cumpon para tu siguiente compra del 20%')
+        } else if (totalCarnes < 100) {
+            alert('Lamentablemente no ganastes un cupon por pobre')
+        }
         break;
 
     default:
