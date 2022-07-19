b03956c952f5c87b9a7dc44ffeb83e7abdcb6460
DaniVillamar <villamar959@gmail.com>
2022-07-18 21:36:17 -0500

    Termino de Entregable 02

--- Entregable-02/JS/main.js
+++ Entregable-02/JS/main.js
@@ -1,55 +1,44 @@
 let comando = prompt(' Que Productos deseas Llevar (Snacks, Cocina, Cena)');
 
+function OPCIONES() {
+    const TOTAL_OPCIONES = prompt('Cuantas Productos vas a llevar: ');
+    let total = 0;
+    for (let i = 0; i < TOTAL_OPCIONES; i++) {
+        console.log('Iteracion' + i);
+        nombre = prompt('Nombre del Prdocuto')
+        total = total + parseInt(prompt('Que precio tiene'));
+    }
+    let totalFrutas = total;
+    alert('El Total de tus Prodcutos es: ' + totalFrutas);
+    if (totalFrutas >= 100) {
+        alert('Ganaste un cumpon para tu siguiente compra del 20%')
+    } else if (totalFrutas < 100) {
+        alert('Lamentablemente no ganastes un cupon por pobre')
+    }
+}
+
+function Agradecimiento() {
+        alert('Gracias por tu pedido, tu pedido se esta armando.')
+}
+
 switch (comando) {
     case 'Snacks':
 
-        const TOTAL_OPCIONES = prompt('Cuantas Productos vas a llevar: ');
-        let total = 0;
-        for (let i = 0; i < TOTAL_OPCIONES; i++) {
-            console.log('Iteracion' + i);
-            nombre = prompt('Nombre del Snack')
-            total = total + parseInt(prompt('Que precio tiene'));
-        }
-        let totalFrutas = total;
-        alert('El Total de tus Prodcutos es: ' + totalFrutas);
-        if (totalFrutas >= 100) {
-            alert('Ganaste un cumpon para tu siguiente compra del 20%')
-        } else if (totalFrutas < 100) {
-            alert('Lamentablemente no ganastes un cupon por pobre')
-        }
+        OPCIONES();
+        Agradecimiento();
+
         break;
     case 'Cocina':
 
-        const TOTAL_OPCIONES2 = prompt('Cuantas Productos vas a llevar: ');
-        let total2 = 0;
-        for (let g = 0; g < TOTAL_OPCIONES2; g++) {
-            console.log('Iteracion' + g);
-            nombreV = prompt('Nombre del Producto')
-            total2 = total2 + parseInt(prompt('Que precio tiene'));
-        }
-        let totalVerduras = total2;
-        alert('El Total de tus Productos es: ' + totalVerduras);
-        if (totalVerduras >= 100) {
-            alert('Ganaste un cumpon para tu siguiente compra del 20%')
-        } else if (totalVerduras < 100) {
-            alert('Lamentablemente no ganastes un cupon por pobre')
-        }
+        OPCIONES();
+        Agradecimiento();
+
         break;
     case 'Cena':
-        const TOTAL_OPCIONES3 = prompt('Cuantas productos vas a llevar: ');
-        let total3 = 0;
-        for (let g = 0; g < TOTAL_OPCIONES3; g++) {
-            console.log('Iteracion' + g);
-            nombre3 = prompt('Nombre del Producto')
-            total3 = total3 + parseInt(prompt('Que precio tiene'));
-        }
-        let totalCarnes = total3;
-        alert('El Total de tus Productos es: ' + totalCarnes);
-        if (totalCarnes >= 100) {
-            alert('Ganaste un cumpon para tu siguiente compra del 20%')
-        } else if (totalCarnes < 100) {
-            alert('Lamentablemente no ganastes un cupon por pobre')
-        }
+        
+        OPCIONES();
+        Agradecimiento();
+
         break;
 
     default:
